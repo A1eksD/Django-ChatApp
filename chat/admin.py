cbd79dcefc16663dedc7c01304211b12c24352a1
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-06-12 13:44:32 +0200

    3th commit

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,15 @@
+
 from django.contrib import admin
+from .models import Chat, Message
+
+class MessageAdmin(admin.ModelAdmin):
+    #Zeige die info beim anklicken an
+    fields = ('text','create_at', 'author', 'receiver')
+    #Zeige die Info als Tabelle beim laden an 
+    list_display = ('text','create_at', 'author', 'receiver')
+    #CustomFilter
+    search_fields = ('text',)
 
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'chat/index.html')
+    if request.method == 'POST':
+        print("Received date " + request.POST['textMessage'])
+    return render(request, 'chat/index.html', {'username': 'Aleks'})
